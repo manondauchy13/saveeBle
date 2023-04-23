03e849d321af6513ccfe2fd999a7e853bcf89fdf
elisa.preynat <elisa.preynat@isen.yncrea.fr>
2023-04-23 17:13:16 +0200

    Add layout file Not working

--- app/src/main/java/com/example/saveeble/ble/BleService.kt
+++ app/src/main/java/com/example/saveeble/ble/BleService.kt
@@ -1,10 +1,8 @@
 package com.example.saveeble.ble
 
 import android.bluetooth.BluetoothGattCharacteristic
-import android.widget.ExpandableListView
+
 
 class BleService (val name: String, val characteristics: MutableList<BluetoothGattCharacteristic>):
-    Expandable(name, characteristics){
+    expandableListView.expandGroup()
 
-
-    }
--- app/src/main/java/com/example/saveeble/ble/DeviceDetailActivity.kt
+++ app/src/main/java/com/example/saveeble/ble/DeviceDetailActivity.kt
@@ -4,15 +4,18 @@ import android.annotation.SuppressLint
 import android.bluetooth.*
 import android.content.ContentValues
 import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.InputType
 import android.util.Log
+import android.widget.Button
 import android.widget.EditText
 import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
-import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+
 import com.example.saveeble.R
 import com.example.saveeble.databinding.ActivityDeviceDetailBinding
 import java.util.*
